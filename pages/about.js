9b7c9f1c621b2449779d739238997a4907d2946b
Imaad Uddin <imaaduddin27@gmail.com>
2021-10-24 01:49:49 -0500

    adding new pages

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is some text here.</p>
+            <p>This is some more text</p>
+        </div>
+    );
+}
+ 
+export default About;
