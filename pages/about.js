9e971678642d3470c294db023f332ba613f0d719
Imaad Uddin <imaaduddin27@gmail.com>
2021-10-25 17:19:26 -0500

    images and linking pages

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from "next/head"
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>This is some text here.</p>
-            <p>This is some more text</p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name='keywords' content='ninjas'></meta>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>This is some text here.</p>
+                <p>This is some more text</p>
+            </div>
+        </>
     );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,23 @@ import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <div>
-      <Navbar/>
-      <h1>Hello!</h1>
-      <p>This is some even more text here.</p>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name='keywords' content='ninjas'></meta>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Hello!</h1>
+        <p className={styles.text}>This is some even more text here.</p>
+        <p className={styles.text}>This is even more text right over here!</p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listings</a>
+        </Link>
+      </div>
+    </>
   )
 }
